cb4d226a8650547c36964735c24c898931ef1c2b
Brennan Cain <brenn.cain@gmail.com>
2016-07-14 14:21:52 -0400

    Banana

--- Bang_Bang/bang_bang.py
+++ Bang_Bang/bang_bang.py
@@ -1,53 +1,115 @@
-#!/usr/bin/python
-
+#!/usr/bin/env python
+# general imports for all python nodes
 import rospy
-from ackermann_msgs import AckermannDrive
-from sensor_msgs import LaserScan
-
-#this holds the answer from the call back each time its called
-global angle=0
-
-#gets the error
-def getError(goal,L,begin,end):
-    return goal-(min(L[begin:end]))
-
-#get the angle
-def getSteeringCmd(error,threshold,fullLeft,fullRight):
-    if(abs(error)<threshold):
-        return 0
-    elif error>0:
-        return fullLeft
-    else:
-        return fullRight
-
-#passed to the subscriber
-def callback(msg):
-    #get the laser information
-    error=getError(.4, msg.ranges, 670, 1080)
-    angle=getSteeringCmd(error, .03, 1, -1)
+import math
+import numpy
+# node specific imports
+from ackermann_msgs.msg import AckermannDriveStamped # steering messages
+from sensor_msgs.msg import LaserScan, Joy # joystick and laser scanner msgs
+class WallE():
     
-
-def converter():
-    rospy.init_node('bang_bang_node')
-    sub = rospy.Subscriber('scan',LaserScan,callback)
-    pub = rospy.Publisher("vesc/ackermann_cmd_mux/input/navigation",AckermannDrive)
-    rate=rospy.rate(10)
-    while not rospy.is_shutdown():
-        msg=AckermannDrive()
-        msg.speed=1.0
-        msg.steering_angle=angle
+    # variable to return distance to wall from callback
+    # (at 90 and 60 degrees to left of vehicle)
+    #scan = [0.0, 0.0] # Ensure it is created before being used
+    steer=0
+    
+    def getError(self,goal,L,begin,end):
+        return goal-(min(L[begin:end]))
+    
+    #get the angle
+    def getSteeringCmd(self,error,threshold,fullLeft,fullRight):
+        if(abs(error)<threshold):
+            return 0
+        elif error>0:
+            return fullLeft
+        else:
+            return fullRight
+    
+    #passed to the subscriber
+    def callback(self,msg):
+        #get the laser information
+        error=self.getError(.4, msg.ranges, 670, 1080)
+        angle=self.getSteeringCmd(error, .03, 1, -1)
         
-        pub.publish(msg)
+    def shutdown(self):
+        rospy.loginfo("Stopping the robot...")
+        # always make sure to leave the robot stopped
+        self.drive.publish(AckermannDriveStamped())
+        rospy.sleep(1)
+    
+    def __init__(self):
+        #setup the node
+        rospy.init_node('wall_bang', anonymous=False)
+        rospy.on_shutdown(self.shutdown)
         
-        rate.sleep()
-
-     
-if __name__ == "__main__":
+        # output messages/sec (also impacts latency)
+        rate = 10 
+        r = rospy.Rate(rate)
+        
+        # node specific topics (remap on command line or in launch file)
+        self.drive = rospy.Publisher('/drive', AckermannDriveStamped, queue_size=5)
+        
+        #sets the subscriber
+        rospy.Subscriber('scan', LaserScan, self.callback)
+        
+        # set control parameters
+        speed = 0.5 # constant travel speed in meters/second
+        dist_wall = 0.333 # start and stay at ~1ft from wall
+        dist_trav = 5.0 # meters to travel in time travel mode
+        
+        # fill out fields in ackermann steering message (to go straight)
+        drive_cmd = AckermannDriveStamped()
+        drive_cmd.drive.speed = speed
+        drive_cmd.drive.steering_angle = 0.0
+        #r60 = dist_wall / math.cos(math.radians(30.0)) # expected distance if parallel to wall
+        
+        # assume correct vehicle pose (at start)
+        #scan = [dist_wall, r60] 
+        
+        # main processing loop (runs for pre-determined duration in time travel mode)
+        time = dist_trav / speed
+        ticks = int(time * rate) # convert drive time to ticks
+        for t in range(ticks):
+            # bang-bang controller for steering direction (single point version - don't use)
+            # turn full left (1.0) or right (-1.0)
+            #drive_cmd.drive.steering_angle = math.copysign(1.0,-(scan[1]-r60))
+            drive_cmd.drive.steering_angle=self.steer
+            
+            self.drive.publish(drive_cmd) # post this message
+            #Chill out for a bit
+            r.sleep() 
+        # always make sure to leave the robot stopped
+        self.drive.publish(AckermannDriveStamped())
+        
+        
+    
+        
+        
+if __name__ == '__main__':
     try:
-        converter()
-    except rospy.ROSInterruptException:
-        pass
+        WallE()
+    except:
+        rospy.loginfo("WallE node terminated.")
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
         
-    
-    
     
